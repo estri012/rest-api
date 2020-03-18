4bcf43ffd55a77e473faf88f172fa6bdbc2fd89e
estri012 <estri.puji.lestari@mail.ugm.ac.id>
2020-03-18 10:39:18 +0700

    chart.js is done

--- gmlews/settings.py
+++ gmlews/settings.py
@@ -49,7 +49,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
         ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 } 
 
 MIDDLEWARE = [
